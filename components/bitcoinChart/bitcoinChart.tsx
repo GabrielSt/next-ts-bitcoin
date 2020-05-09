9f42579c8198e3bda740681f3f500b7371a4fc92
Gabriel Pereira <gabriel.st@outlook.com>
2020-05-09 01:19:28 +0100

    Adjust styles to mobile devices

--- components/bitcoinChart/bitcoinChart.tsx
+++ components/bitcoinChart/bitcoinChart.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Chart } from "react-charts";
+import { useMediaQuery } from "react-responsive";
 
 type propTypes = {
-    hist: Array<Array< string | number>>;
-  };
+  hist: Array<Array<string | number>>;
+};
 
 const BitcoinChart = ({ hist }: propTypes) => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+
+  const totalHist = isTabletOrMobile ? hist.slice(20) : hist;
   const data = React.useMemo(
     () => [
       {
         label: "Price in USD",
-        data: hist,
+        data: totalHist,
       },
     ],
     []
@@ -29,15 +33,15 @@ const BitcoinChart = ({ hist }: propTypes) => {
     // space of its parent element automatically
     <div
       style={{
-        width: "75rem",
+        width: "90vw",
         height: "300px",
         margin: "auto",
       }}
     >
-        Daily quotation in USD
+      Daily quotation in USD
       <Chart data={data} axes={axes} tooltip dark />
     </div>
   );
-}
+};
 
 export default BitcoinChart;
